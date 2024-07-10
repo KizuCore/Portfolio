47f50cb2a7faa0c3d115543819f52f05902f0a79
Théo <theoguerinBZH@hotmail.com>
2024-07-10 16:56:05 +0200

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 function AboutCard() {
-  return (
+  return ( 
     <Card className="quote-card-view">
       <Card.Body>
         <blockquote className="blockquote mb-0">

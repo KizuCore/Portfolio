673e9105c0223e1e107e2f59b35bc216918f9985
Théo <theoguerinBZH@hotmail.com>
2024-07-10 15:56:36 +0200

    Update ProjectCards.js

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -2,6 +2,22 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { BsGithub, BsYoutube } from "react-icons/bs";
+import { Container, Row, Col } from "react-bootstrap";
+import { DiCss3, DiHtml5, DiJava, DiJavascript, DiMysql, DiPhp } from "react-icons/di";
+import { SiExpress, SiFlutter, SiKotlin, SiVuedotjs } from "react-icons/si";
+
+const techIcons = {
+  Flutter: <SiFlutter />,
+  MySQL: <DiMysql />,
+  Express: <SiExpress />,
+  Kotlin: <SiKotlin />,
+  Php: <DiPhp />,
+  Java: <DiJava />,
+  Html: <DiHtml5 />,
+  Css: <DiCss3 />,
+  Javascript: <DiJavascript />,
+  Vuejs: <SiVuedotjs />
+};
 
 function ProjectCards(props) {
   return (
@@ -9,6 +25,15 @@ function ProjectCards(props) {
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
         <Card.Title><strong className="blue">{props.title}</strong></Card.Title>
+        
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          {props.techStack.map((tech, index) => (
+            <Col key={index} xs={4} md={2} className="tech-icons2">
+              {techIcons[tech]} <span>{tech.replace(/([a-z])([A-Z])/g, '$1 $2')}</span>
+            </Col>
+          ))}
+        </Row>
+
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
@@ -18,13 +43,11 @@ function ProjectCards(props) {
         </Button>
         {"\n"}
         {"\n"}
-        
-
-        {}
+        { }
 
         {!props.isGitLab && props.youtubeLink && (
           <Button
-            variant="primary"
+            variant="danger"
             href={props.youtubeLink}
             target="_blank"
             style={{ marginLeft: "10px" }}
@@ -37,4 +60,5 @@ function ProjectCards(props) {
     </Card>
   );
 }
+
 export default ProjectCards;

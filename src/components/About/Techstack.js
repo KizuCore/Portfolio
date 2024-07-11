d5d6749e4aab2abde30d3b54ece43a23f9e898d2
Théo <theoguerinBZH@hotmail.com>
2024-07-11 10:56:42 +0200

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
+import ReactTooltip from "react-tooltip";
 import {
   DiJavascript1,
   DiReact,
   DiNodejs,
   DiMysql,
   DiPython,
-  DiJava,
   DiDjango,
   DiScala,
   DiPhp,
 } from "react-icons/di";
-import { FaVuejs } from "react-icons/fa";
+import { FaJava, FaVuejs } from "react-icons/fa";
 import {
   SiFlutter,
   SiTailwindcss,
@@ -19,52 +19,41 @@ import {
   SiExpress,
 } from "react-icons/si";
 
+const icons = [
+  { component: SiFlutter, name: "Flutter" },
+  { component: DiMysql, name: "MySQL" },
+  { component: SiTailwindcss, name: "Tailwind CSS" },
+  { component: SiBootstrap, name: "Bootstrap" },
+  { component: SiExpress, name: "Express" },
+  { component: DiPhp, name: "PHP" },
+  { component: DiScala, name: "Scala" },
+  { component: DiDjango, name: "Django" },
+  { component: DiJavascript1, name: "JavaScript" },
+  { component: DiNodejs, name: "Node.js" },
+  { component: DiReact, name: "React" },
+  { component: DiPython, name: "Python" },
+  { component: FaJava, name: "Java" },
+  { component: FaVuejs, name: "Vue.js" },
+];
+
 function Techstack() {
   return (
-    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFlutter />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMysql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiTailwindcss />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiBootstrap />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiExpress />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPhp />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiScala />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiDjango />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJava />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <FaVuejs />
-      </Col>
-    </Row>
+    <div>
+      <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+        {icons.map((icon, index) => (
+          <Col
+            key={index}
+            xs={4}
+            md={2}
+            className="tech-icons"
+            data-tip={icon.name}
+          >
+            <icon.component />
+          </Col>
+        ))}
+      </Row>
+      <ReactTooltip />
+    </div>
   );
 }
 

2014dfe38864e9898e51e209306c7cf93f59322c
Théo <theoguerinBZH@hotmail.com>
2024-07-11 10:54:27 +0200

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,44 +1,35 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+
 function AboutCard() {
-  return ( 
+  return (
     <Card className="quote-card-view">
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Bonjour à tous, je suis <span className="blue">Théo Guérin </span>
-            de <span className="blue"> Rennes, France.</span>
+            Bonjour à tous, je suis <span className="blue">Théo Guérin</span> de
+            <span className="blue"> Rennes, France.</span>
             <br />
             <br />
             Je suis actuellement <span className="blue">Développeur web Full-Stack</span> de 23 ans en alternance.
             <br />
             <br />
             J'ai actuellement :
-            <br/>
-            <br/>
+            <br />
             <ul>
-              <li className="about-activity"> Licence Informatique-Electronique (Rennes ISTIC)</li> 
-              <li className="about-activity"> Bachelor Développeur-Web (Rennes MyDigitalSchool)</li> 
-              <li className="about-activity"> Titre Concepteur Développeur d'application</li>
+              <li className="about-activity">Licence Informatique-Electronique (Rennes ISTIC)</li>
+              <li className="about-activity">Bachelor Développeur-Web (Rennes MyDigitalSchool)</li>
+              <li className="about-activity">Titre Concepteur Développeur d'application</li>
             </ul>
             <br />
             En dehors du codage, voici quelques autres activités que j'aime faire :
           </p>
           <ul>
-            <li className="about-activity">
-              ✈️ • Voyager
-            </li>
-            <li className="about-activity">
-              📷 • Prendre des photos
-            </li>
-            <li className="about-activity">
-              🔭 • Astronomie
-            </li>
-            <li className="about-activity">
-              🐈‍⬛ • M'occuper d'animaux
-            </li>
+            <li className="about-activity">✈️ • Voyager</li>
+            <li className="about-activity">📷 • Prendre des photos</li>
+            <li className="about-activity">🔭 • Astronomie</li>
+            <li className="about-activity">🐈‍⬛ • M'occuper d'animaux</li>
           </ul>
-
         </blockquote>
       </Card.Body>
     </Card>

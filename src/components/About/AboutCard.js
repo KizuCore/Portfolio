fa11606a9c054c3e090615715b4afb52ee49fc62
Theo35000 <theo@nahibu.com>
2024-08-30 11:30:47 +0200

    Update translation

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -10,18 +10,27 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            {t('greeting')} <span className="blue">Théo Guérin</span> {t('from')}
-            <span className="blue"> Rennes, France.</span>
+            {t('greeting')} <span className="blue">Théo Guérin</span>{t('from')}
+            <span className="blue"> {t('rennes')}</span>.
             <br />
             <br />
-            {t('current_position')}
+            {t('current_position1')} <span className="blue">{t('developper23')}</span>{t('current_position2')} <span className="blue">{t('firstmaster')}</span>{t('current_position3')}
             <br />
             <br />
             {t('qualifications')}
             <br />
             <ul>
-              <li className="about-activity">• {t('degree1')}</li>
-              <li className="about-activity">• {t('degree2')}</li>
+              <li className="about-activity">
+                • <a href="https://istic.univ-rennes.fr/licence-informatique-parcours-informatique" style={{ textDecoration: 'none', color: 'inherit' }} target="_blank" rel="noopener noreferrer">
+                  {t('degree1')}
+                </a>
+              </li>
+              
+              <li className="about-activity">
+                • <a href="https://www.mydigitalschool.com/bachelor-1-2-web" style={{ textDecoration: 'none', color: 'inherit' }} target="_blank" rel="noopener noreferrer">
+                  {t('degree2')}
+                </a>
+              </li>
               <li className="about-activity">
                 • <a href="https://www.francecompetences.fr/recherche/rncp/37873/" style={{ textDecoration: 'none', color: 'inherit' }} target="_blank" rel="noopener noreferrer">
                   {t('degree3')}

8a6ab3004580be525b6edb48aa3fb6d6334a00e3
Theo35000 <theo@nahibu.com>
2024-10-21 11:36:22 +0200

    add new design + effect

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,47 +1,73 @@
 import React from "react";
 import { useTranslation } from 'react-i18next';
 import Card from "react-bootstrap/Card";
+import { useInView } from 'react-intersection-observer';
 
 function AboutCard() {
   const { t } = useTranslation();
 
+  const { ref: refText, inView: textInView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
   return (
-    <Card className="quote-card-view">
+    <Card className="quote-card-view mt-4">
       <Card.Body>
-        <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
-            {t('greeting')} <span className="blue">Théo Guérin</span>{t('from')}
-            <span className="blue"> {t('rennes')}</span>.
+        <blockquote
+          className={`blockquote mb-0 background-box ${textInView ? 'slide-in-left' : ''}`}
+          ref={refText}
+        >
+          {/* Justification texte*/}
+          <p className="text-justify">
+            {t('greeting')} <span className="blue">Théo Guérin</span>{t('from')}<span className="blue"> {t('rennes')}</span>.
             <br />
             <br />
             {t('current_position1')} <span className="blue">{t('developper23')}</span>{t('current_position2')} <span className="blue">{t('firstmaster')}</span>{t('current_position3')}
             <br />
             <br />
             {t('qualifications')}
-            <br />
-            <ul>
-              <li className="about-activity">
-                • <a href="https://istic.univ-rennes.fr/licence-informatique-parcours-informatique" style={{ textDecoration: 'none', color: 'inherit' }} target="_blank" rel="noopener noreferrer">
-                  {t('degree1')}
-                </a>
-              </li>
-              
-              <li className="about-activity">
-                • <a href="https://www.mydigitalschool.com/bachelor-1-2-web" style={{ textDecoration: 'none', color: 'inherit' }} target="_blank" rel="noopener noreferrer">
-                  {t('degree2')}
-                </a>
-              </li>
-              <li className="about-activity">
-                • <a href="https://www.francecompetences.fr/recherche/rncp/37873/" style={{ textDecoration: 'none', color: 'inherit' }} target="_blank" rel="noopener noreferrer">
-                  {t('degree3')}
-                </a>
-              </li>
+          </p>
 
-            </ul>
-            <br />
+          {/* Liste */}
+          <ul className="mt-3">
+            <li className="about-activity mb-2">
+              • <a
+                href="https://istic.univ-rennes.fr/licence-informatique-parcours-informatique"
+                className="text-decoration-none blue"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {t('degree1')}
+              </a>
+            </li>
+            <li className="about-activity mb-2">
+              • <a
+                href="https://www.mydigitalschool.com/bachelor-1-2-web"
+                className="text-decoration-none blue"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {t('degree2')}
+              </a>
+            </li>
+            <li className="about-activity mb-2">
+              • <a
+                href="https://www.francecompetences.fr/recherche/rncp/37873/"
+                className="text-decoration-none blue"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {t('degree3')}
+              </a>
+            </li>
+          </ul>
+
+          {/* Section Hobbies */}
+          <p className="text-justify mt-4">
             {t('outside_of_coding')}
           </p>
-          <ul>
+          <ul className="list-unstyled mt-3">
             <li className="about-activity">{t('hobby1')}</li>
             <li className="about-activity">{t('hobby2')}</li>
             <li className="about-activity">{t('hobby3')}</li>

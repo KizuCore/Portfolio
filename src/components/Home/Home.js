d86c8748b089062932114e005c27ad91f740a74c
Theo35000 <theo@nahibu.com>
2024-10-21 15:47:46 +0200

    add animation + mobile

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,9 +5,15 @@ import homeLogo from "../../Assets/home/home-main.webp";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
+import { useInView } from 'react-intersection-observer';
 
 function Home() {
   const { t } = useTranslation();
+
+  const { ref: refImg, inView: imgInView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
 
   return (
     <section>
@@ -15,27 +21,25 @@ function Home() {
         <Particle />
         <Container className="home-content">
           <Row className="align-items-center">
-            
-            <Col md={7} className="home-header slide-in-left text-center text-md-left py-5">
-              <h1 className="heading mb-3">
+            {/* Colonne pour le texte*/}
+            <Col md={7} className="home-header slide-in-left d-flex flex-column justify-content-center text-center text-md-left py-5">
+              <h1 className="heading mb-3 title-font">
                 {t('hello')}{" "}
-                <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
-                </span>
+                <span className="wave" role="img" aria-labelledby="wave">👋🏻</span>
               </h1>
 
-              <h1 className="heading-name mb-3">
+              <h1 className="heading-name mb-3 title-font">
                 {t('iam')}
                 <strong className="blue-title"> {t('name')}</strong>
               </h1>
-
-              <div className="pt-3 blue-title">
-                <Type />
+          
+              <div className="pt-3 blue-title d-flex justify-content-center align-items-center">
+               <Type />
               </div>
             </Col>
 
-            
-            <Col md={5} className="d-flex justify-content-center py-5">
+            {/* Colonne pour l'image */}
+            <Col md={5} className={`d-flex justify-content-center py-5 ${imgInView ? 'slide-in-right' : ''}`} ref={refImg}>
               <img
                 src={homeLogo}
                 alt="home pic"

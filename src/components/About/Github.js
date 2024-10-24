65122aadc7c6ed91d0dc9929d9b05ee777bdf2aa
Theo35000 <theo@nahibu.com>
2024-10-24 14:44:56 +0200

    add github

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import GitHubCalendar from "react-github-calendar";
 import { Row, Col } from "react-bootstrap";
 import { FaGitlab } from "react-icons/fa";
+import { SiGithub } from "react-icons/si";
 
 function Github() {
   const { t } = useTranslation();
@@ -11,9 +12,20 @@ function Github() {
     <div>
       {/* Section GitHub */}
       <Row className="justify-content-center mb-4">
-        <h2 className="project-heading mb-5">
+        <h2 className="project-heading mb-4">
           <strong className="blue-title">{t('github_profile')}</strong>
         </h2>
+        <Col md={6} className="text-center pb-4">
+          <a
+            href="https://github.com/Theo22100/"
+            target="_blank"
+            rel="noreferrer"
+            className="github-link" 
+            aria-label={t('github_profile_seo')}
+          >
+            <SiGithub size={70} className="cursor-pointer"/>
+          </a>
+        </Col>
         <GitHubCalendar
           username="Theo22100"
           blockSize={15}

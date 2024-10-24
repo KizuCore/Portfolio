08c14c7bf9b5b369212256a2a2ffa2a8bbe8d087
Theo35000 <theo@nahibu.com>
2024-10-24 14:45:41 +0200

    add more langages +  favorite

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -13,7 +13,7 @@ import {
   DiPhp,
   DiWordpress,
 } from "react-icons/di";
-import { FaJava, FaVuejs } from "react-icons/fa";
+import { FaFileExcel, FaJava, FaVuejs } from "react-icons/fa";
 import {
   SiFlutter,
   SiTailwindcss,
@@ -22,30 +22,37 @@ import {
   SiCss3,
   SiLaravel,
   SiNestjs,
-  SiFlask
+  SiFlask,
+  SiCplusplus,
+  SiGnubash,
+  SiKotlin
 } from "react-icons/si";
 
-// Technologies avec niveau (0=Novice, 1=Intermédiaire, 2=Expérimenté)
+// Technologies avec niveau (0=Novice, 1=Intermédiaire, 2=Expérimenté, 3=Favori)
 const icons = [
-  { component: SiFlutter, name: "Flutter", level: 2 },
+  { component: SiFlutter, name: "Flutter", level: 3 },
+  { component: DiReact, name: "React", level: 3 },
+  { component: SiExpress, name: "Express", level: 3 },
   { component: DiMysql, name: "MySQL", level: 2 },
-  { component: SiTailwindcss, name: "Tailwind CSS", level: 1 },
   { component: SiBootstrap, name: "Bootstrap", level: 2 },
-  { component: SiExpress, name: "Express", level: 2 },
   { component: DiPhp, name: "PHP", level: 2 },
   { component: DiScala, name: "Scala", level: 2 },
-  { component: DiDjango, name: "Django", level: 1 },
   { component: DiJavascript1, name: "JavaScript", level: 2 },
   { component: DiNodejs, name: "Node.js", level: 2 },
-  { component: DiReact, name: "React", level: 2 },
   { component: DiPython, name: "Python", level: 2 },
   { component: FaJava, name: "Java", level: 2 },
-  { component: FaVuejs, name: "Vue.js", level: 0 },
-  { component: DiWordpress, name: "Wordpress", level: 1 },
+  { component: DiWordpress, name: "Wordpress", level: 2 },
   { component: SiCss3, name: "CSS", level: 2 },
+  { component: SiTailwindcss, name: "Tailwind CSS", level: 1 },
+  { component: DiDjango, name: "Django", level: 1 },
+  { component: SiKotlin, name: "Kotlin", level: 1 },
+  { component: FaVuejs, name: "Vue.js", level: 0 },
   { component: SiFlask, name: "Flask", level: 0 },
   { component: SiLaravel, name: "Laravel", level: 0 },
-  { component: SiNestjs, name: "Nest.js", level: 0 }
+  { component: SiGnubash, name: "Bash", level: 0 },
+  { component: SiCplusplus, name: "C++", level: 0 },
+  { component: FaFileExcel, name: "VBA", level: 0 },
+  { component: SiNestjs, name: "Nest.js", level: 0 },
 ];
 
 function Techstack() {
@@ -58,7 +65,7 @@ function Techstack() {
 
           // Déterminer classe CSS avec niveau
           const levelClass =
-            icon.level === 0 ? "border-blue" : icon.level === 1 ? "border-yellow" : "border-green";
+            icon.level === 0 ? "border-orange" : icon.level === 1 ? "border-yellow" :  icon.level === 2 ? "border-green" : "border-blue";
 
           return (
             <Col
@@ -66,8 +73,7 @@ function Techstack() {
               xs={4}
               md={2}
               className={`tech-icons ${levelClass}`} 
-              data-tip={`${icon.name} - ${t('level')} ${icon.level === 0 ? t('novice') : icon.level === 1 ? t('intermediate') : t('advanced')}`}
-
+              data-tip={`${icon.name} - ${icon.level === 0 ? t('novice') : icon.level === 1 ? t('intermediate') : icon.level === 2 ? t('advanced') : t('favorite')}`}
             >
               <IconComponent aria-label={icon.name} />
             </Col>

769a6130473962fb700ec133fabade9bdb2ba23e
Theo22100 <theoguerinBZH@hotmail.com>
2024-10-27 10:38:47 +0100

    fix bug

--- src/components/Header/LanguageSelector.js
+++ src/components/Header/LanguageSelector.js
@@ -5,7 +5,7 @@ import { FaAngleDown } from 'react-icons/fa';
 import { useTranslation } from 'react-i18next';
 
 function LanguageSelector() {
-  const { i18n } = useTranslation();
+  const { i18n, t } = useTranslation();
 
   const changeLanguage = (lang) => {
     i18n.changeLanguage(lang);
@@ -19,16 +19,16 @@ function LanguageSelector() {
   return (
     <Dropdown className="language-selector">
       <Dropdown.Toggle variant="secondary" id="dropdown-basic" aria-label="Language Selector">
-        <Flag code={i18n.language === "fr" ? "FR" : "GB"} height="auto" width="25" style={{ marginBottom: "2px" }} />{' '}
+        <Flag code={i18n.language === "fr" ? "FR" : "GB"} height="auto" width="25" style={{ marginBottom: "2px" }} alt={t(i18n.language === "fr" ? "flag_fr" : "flag_en")} />{' '}
         {i18n.language === "fr" ? "FR" : "EN"} <FaAngleDown />
       </Dropdown.Toggle>
 
       <Dropdown.Menu>
         <Dropdown.Item onClick={() => changeLanguage("en")}>
-          <Flag code="GB" width="25" height="auto" style={{ marginRight: "10px" }} alt={i18n('flag_en')} /> EN
+          <Flag code="GB" width="25" height="auto" style={{ marginRight: "10px" }} alt={t("flag_en")} /> EN
         </Dropdown.Item>
         <Dropdown.Item onClick={() => changeLanguage("fr")}>
-          <Flag code="FR" width="25" height="auto" style={{ marginRight: "10px" }} alt={i18n('flag_fr')} /> FR
+          <Flag code="FR" width="25" height="auto" style={{ marginRight: "10px" }} alt={t("flag_fr")} /> FR
         </Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>

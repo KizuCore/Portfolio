f06db3ba69d7f220ee7e16ea1c74460dc639e0ff
Theo22100 <theoguerinBZH@hotmail.com>
2024-10-27 15:24:30 +0100

    test

--- src/components/Utils/Particle.js
+++ src/components/Utils/Particle.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
-import Particles from "react-tsparticles";
+import React, { useEffect, useState, Suspense } from "react";
+
+const Particles = React.lazy(() => import("react-tsparticles"));
 
 function Particle() {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
@@ -13,54 +14,56 @@ function Particle() {
   }, []);
 
   return (
-    <Particles
-      id="tsparticles"
-      aria-hidden="true"
-      role="presentation"
-      params={{
-        particles: {
-          number: {
-            value: isMobile ? 100 : 300,
-            density: {
-              enable: true,
-              value_area: 1000,
+    <Suspense fallback={<div>Loading...</div>}>
+      <Particles
+        id="tsparticles"
+        aria-hidden="true"
+        role="presentation"
+        params={{
+          particles: {
+            number: {
+              value: isMobile ? 50 : 300,
+              density: {
+                enable: true,
+                value_area: 1000,
+              },
+            },
+            line_linked: {
+              enable: false,
+              opacity: 0.02,
+            },
+            move: {
+              direction: "right",
+              speed: 0.05,
+            },
+            size: {
+              value: 1,
+            },
+            opacity: {
+              anim: {
+                enable: true,
+                speed: 1.5,
+                opacity_min: 0.05,
+              },
             },
           },
-          line_linked: {
-            enable: false,
-            opacity: 0.02,
-          },
-          move: {
-            direction: "right",
-            speed: 0.05,
-          },
-          size: {
-            value: 1,
-          },
-          opacity: {
-            anim: {
-              enable: true,
-              speed: 1.5,
-              opacity_min: 0.05,
+          interactivity: {
+            events: {
+              onclick: {
+                enable: true,
+                mode: "push",
+              },
+            },
+            modes: {
+              push: {
+                particles_nb: 1,
+              },
             },
           },
-        },
-        interactivity: {
-          events: {
-            onclick: {
-              enable: true,
-              mode: "push",
-            },
-          },
-          modes: {
-            push: {
-              particles_nb: 1,
-            },
-          },
-        },
-        retina_detect: true,
-      }}
-    />
+          retina_detect: true,
+        }}
+      />
+    </Suspense>
   );
 }
 

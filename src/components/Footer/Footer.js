779663ec9938c1d385a3e69915d3a5410f025fe1
Theo22100 <theoguerinBZH@hotmail.com>
2024-10-28 13:18:35 +0100

    edit mobile

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,9 @@
 import React from "react";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 import { Container, Row, Col } from "react-bootstrap";
-import {
-  AiFillGithub,
-  AiOutlineMail,
-} from "react-icons/ai";
+import { AiFillGithub, AiOutlineMail } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
-import '../../Assets/style/Footer/Footer.css'; 
+import "../../Assets/style/Footer/Footer.css";
 
 function Footer() {
   const { t } = useTranslation();
@@ -16,24 +13,26 @@ function Footer() {
   return (
     <footer className="footer">
       <Container fluid>
-        <Row>
-          <Col md="4" className="footer-copywright">
+        <Row className="text-center text-md-left">
+          <Col xs="12" md="4" className="footer-copywright py-2">
             <h3>
-              {t('bytheo1')}{" "}
-              <a 
+              {t("bytheo1")}{" "}
+              <a
                 href="https://github.com/Theo22100"
                 target="_blank"
                 rel="noopener noreferrer"
-                aria-label={t('github_profile_link')}
+                aria-label={t("github_profile_link")}
               >
                 Theo22100
               </a>
             </h3>
           </Col>
-          <Col md="4" className="footer-copywright">
-            <h3>Copyright © {year} - {t('portfolio_theo')}</h3>
+          <Col xs="12" md="4" className="footer-copywright py-2">
+            <h3>
+              Copyright © {year} - {t("portfolio_theo")}
+            </h3>
           </Col>
-          <Col md="4" className="footer-body">
+          <Col xs="12" md="4" className="footer-body py-2">
             <ul className="footer-icons">
               <li className="social-icons-footer">
                 <a
@@ -41,31 +40,29 @@ function Footer() {
                   style={{ color: "white" }}
                   target="_blank"
                   rel="noopener noreferrer"
-                  aria-label={t('github_profile_link')}
+                  aria-label={t("github_profile_link")}
                 >
                   <AiFillGithub />
                 </a>
               </li>
-
               <li className="social-icons-footer">
                 <a
                   href="https://www.linkedin.com/in/théo-guérin-b20630192/"
                   style={{ color: "white" }}
                   target="_blank"
                   rel="noopener noreferrer"
-                  aria-label={t('linkedin_profile_link')}
+                  aria-label={t("linkedin_profile_link")}
                 >
                   <FaLinkedinIn />
                 </a>
               </li>
-
               <li className="social-icons-footer">
                 <a
                   href="mailto:theo.guerin35000@gmail.com"
                   style={{ color: "white" }}
                   target="_blank"
                   rel="noopener noreferrer"
-                  aria-label={t('email_link')}
+                  aria-label={t("email_link")}
                 >
                   <AiOutlineMail />
                 </a>

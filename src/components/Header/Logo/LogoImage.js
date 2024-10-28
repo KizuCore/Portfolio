020b619e891228f746ed6fd4173b0c9ffa0fd6c9
Theo22100 <theoguerinBZH@hotmail.com>
2024-10-28 11:47:00 +0100

    edit animation

--- src/components/Header/Logo/LogoImage.js
+++ src/components/Header/Logo/LogoImage.js
@@ -1,38 +1,48 @@
 import React, { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
 import logo from "../../../Assets/images/logo.webp";
 import logoWhite from "../../../Assets/images/logo_white.webp"; 
 import '../../../Assets/style/Easter/style_easter.css'; 
 import '../../../Assets/style/Header/header.css';
 
 function LogoImage({ isAnimating, t }) {
-  const [isLogoWhite, setIsLogoWhite] = useState(false);
+  const [showWhiteLogo, setShowWhiteLogo] = useState(false);
 
   useEffect(() => {
-    let logoTimeout;
-
     if (isAnimating) {
-      logoTimeout = setTimeout(() => {
-        setIsLogoWhite(true); // Affiche logo blanc après 5s
+      // Change l'image après 5 secondes
+      const logoTimeout = setTimeout(() => {
+        setShowWhiteLogo(true);
       }, 5000);
-    } else {
-      // Remettre logo noir
-      setIsLogoWhite(false);
-    }
 
-    return () => {
-      if (logoTimeout) {
-        clearTimeout(logoTimeout);
-      }
-    };
-  }, [isAnimating]); 
+      // Nettoie le timeout lorsque le composant est démonté
+      return () => clearTimeout(logoTimeout);
+    } else {
+      // Si isAnimating est désactivé, remet le logo noir
+      setShowWhiteLogo(false);
+    }
+  }, [isAnimating]);
 
   return (
-    <>
-      {/* Logo noir */}
-      <img src={logo} className={`img-fluid logo ${isLogoWhite ? 'fade-out' : 'fade-in'}`} alt={t('brand')} loading="lazy" decoding="async"/>
-      {/* Logo blanc */}
-      <img src={logoWhite} className={`img-fluid logo logo-white ${isLogoWhite ? 'fade-in' : 'fade-out'}`} alt={t('brand')} loading="lazy" decoding="async" />
-    </>
+    <motion.div
+      className="logo-container"
+      style={{ position: 'relative' }}
+      initial={{ opacity: 0, scale: 0.8 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5, ease: "easeInOut" }}
+    >
+      <motion.img
+        src={showWhiteLogo ? logoWhite : logo}
+        alt={t('brand')}
+        className="img-fluid logo"
+        loading="lazy"
+        decoding="async"
+        style={{ width: '100%', height: 'auto' }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+      />
+    </motion.div>
   );
 }
 

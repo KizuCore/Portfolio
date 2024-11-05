bc18ff43b69ba4692274ae73ab2bfd5701858ed6
Theo22100 <theoguerinBZH@hotmail.com>
2024-11-06 00:22:54 +0100

    remove speedinsights

--- src/index.js
+++ src/index.js
@@ -5,13 +5,11 @@ import App from './App';
 import reportWebVitals from './tests/reportWebVitals';
 import './i18n';
 import Pre from './components/Utils/Pre';
-import { SpeedInsights } from '@vercel/speed-insights/react';
 
 ReactDOM.render(
   <React.StrictMode>
     <Suspense fallback={<Pre />}>
       <App />
-      <SpeedInsights/>
     </Suspense>
   </React.StrictMode>,
   document.getElementById('root')

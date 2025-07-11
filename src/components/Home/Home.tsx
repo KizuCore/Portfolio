49730cec38f7feee624d226ab11e3d3f58117b99
Theo35000 <theo@nahibu.com>
2025-07-11 09:08:18 +0200

    fix: improve responsiveness of Home component by conditionally rendering elements based on screen size

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { JSX } from "react";
+import { JSX, useEffect, useState } from "react";
 import { useTranslation } from 'react-i18next';
 import { Container, Row, Col } from "react-bootstrap";
 import { motion } from 'framer-motion';
@@ -12,11 +12,22 @@ import { useInView } from 'react-intersection-observer';
 
 function Home(): JSX.Element {
   const { t } = useTranslation();
+  const [isMobile, setIsMobile] = useState(false);
 
   const { ref, inView: imgInView } = useInView({
     triggerOnce: true,
     threshold: 0.1,
   });
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768); // <768px = mobile
+    };
+
+    handleResize(); // exécute au chargement
+    window.addEventListener("resize", handleResize); // met à jour si resize
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <section>
@@ -26,52 +37,82 @@ function Home(): JSX.Element {
           <Row className="align-items-center justify-content-center text-center text-md-left" style={{ paddingBottom: "15em", paddingTop: "4em" }}>
             {/* Texte */}
             <Col xs={12} md={6} className="home-header d-flex flex-column justify-content-center text-center text-md-left py-5">
-              <motion.div
-                initial={{ opacity: 0, x: -50 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ duration: 0.8, ease: "easeOut" }}
-                className="pb-5"
-              >
-                <h1 className="heading mb-3 title-font">
-                  {t('hello')}{" "}<span className="wave" role="img" aria-labelledby="wave">👋🏻</span>
-                </h1>
-                <h2 className="heading-name styled-title">
-                  {t('iam')}<span className="gradient-name"> {t('nametheo')}</span>
-                </h2>
+              {isMobile ? (
+                <div className="pb-5">
+                  <h1 className="heading mb-3 title-font">
+                    {t('hello')}{" "}
+                    <span className="wave" role="img" aria-labelledby="wave">👋🏻</span>
+                  </h1>
+                  <h2 className="heading-name styled-title">
+                    {t('iam')}<span className="gradient-name"> {t('nametheo')}</span>
+                  </h2>
+                </div>
+              ) : (
+                <motion.div
+                  initial={{ opacity: 0, x: -50 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  transition={{ duration: 0.8, ease: "easeOut" }}
+                  className="pb-5"
+                >
+                  <h1 className="heading mb-3 title-font">
+                    {t('hello')}{" "}
+                    <span className="wave" role="img" aria-labelledby="wave">👋🏻</span>
+                  </h1>
+                  <h2 className="heading-name styled-title">
+                    {t('iam')}<span className="gradient-name"> {t('nametheo')}</span>
+                  </h2>
+                </motion.div>
+              )}
 
-              </motion.div>
 
               {/* Développeur... */}
-              <motion.div
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.8, ease: "easeOut", delay: 0.3 }}
-                className="pt-3 pb-5 blue-title d-flex justify-content-center align-items-center"
-              >
-                <TypeDev />
-              </motion.div>
+              {isMobile ? (
+                <div className="pt-3 pb-5 blue-title d-flex justify-content-center align-items-center">
+                  <TypeDev />
+                </div>
+              ) : (
+                <motion.div
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.8, ease: "easeOut", delay: 0.3 }}
+                  className="pt-3 pb-5 blue-title d-flex justify-content-center align-items-center"
+                >
+                  <TypeDev />
+                </motion.div>
+              )}
+
             </Col>
             {/* Colonne pour l'image */}
             <Col ref={ref} xs={12} md={6} className="d-flex justify-content-center align-items-center py-4">
               <div className="logo-wrapper">
                 <Tilt>
-                  <motion.img
-                    src={LogoDeveloper}
-                    className="img-fluid"
-                    alt={t('theo_developer')}
-                    width="400"
-                    height="400"
-                    loading="eager"
-                    decoding="sync"
-                    initial={{ opacity: 0, x: 50 }}
-                    animate={imgInView ? { opacity: 1, x: 0 } : {}}
-                    transition={{ duration: 0.8, ease: "easeOut" }}
-                  />
+                  {isMobile ? (
+                    <img
+                      src={LogoDeveloper}
+                      className="img-fluid"
+                      alt={t('theo_developer')}
+                      width="400"
+                      height="400"
+                      loading="eager"
+                      decoding="sync"
+                    />
+                  ) : (
+                    <motion.img
+                      src={LogoDeveloper}
+                      className="img-fluid"
+                      alt={t('theo_developer')}
+                      width="400"
+                      height="400"
+                      loading="eager"
+                      decoding="sync"
+                      initial={{ opacity: 0, x: 50 }}
+                      animate={imgInView ? { opacity: 1, x: 0 } : {}}
+                      transition={{ duration: 0.8, ease: "easeOut" }}
+                    />
+                  )}
                 </Tilt>
               </div>
             </Col>
-
-
           </Row>
         </Container>
       </Container>

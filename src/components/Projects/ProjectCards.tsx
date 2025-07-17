bbf1e416004ce31a2ecd1170ef3c37f734f9cc31
Theo35000 <theo@nahibu.com>
2025-07-17 10:26:52 +0200

    style: refactor project card styles and improve layout for better responsiveness

--- src/components/Projects/ProjectCards.tsx
+++ src/components/Projects/ProjectCards.tsx
@@ -1,35 +1,33 @@
+// ProjectCards.tsx (refactoré)
 import React, { Suspense } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import { Row, Col, Spinner } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import { useTranslation } from 'react-i18next';
 import '../../assets/styles/Projet/Projet.css';
 
-// Chargement différé des icônes
 const BsGithub = React.lazy(() => import("react-icons/bs").then(module => ({ default: module.BsGithub })));
 const BsYoutube = React.lazy(() => import("react-icons/bs").then(module => ({ default: module.BsYoutube })));
 
 const techIcons: Record<string, React.LazyExoticComponent<React.ComponentType<any>>> = {
-  Flutter: React.lazy(() => import("react-icons/si").then(module => ({ default: module.SiFlutter }))),
-  MySQL: React.lazy(() => import("react-icons/di").then(module => ({ default: module.DiMysql }))),
-  Express: React.lazy(() => import("react-icons/si").then(module => ({ default: module.SiExpress }))),
-  Kotlin: React.lazy(() => import("react-icons/si").then(module => ({ default: module.SiKotlin }))),
-  Php: React.lazy(() => import("react-icons/di").then(module => ({ default: module.DiPhp }))),
-  Java: React.lazy(() => import("react-icons/fa").then(module => ({ default: module.FaJava }))),
-  Html: React.lazy(() => import("react-icons/di").then(module => ({ default: module.DiHtml5 }))),
-  Css: React.lazy(() => import("react-icons/di").then(module => ({ default: module.DiCss3 }))),
-  Javascript: React.lazy(() => import("react-icons/di").then(module => ({ default: module.DiJavascript1 }))),
-  Vuejs: React.lazy(() => import("react-icons/si").then(module => ({ default: module.SiVuedotjs }))),
-  React: React.lazy(() => import("react-icons/di").then(module => ({ default: module.DiReact }))),
-  Bootstrap: React.lazy(() => import("react-icons/di").then(module => ({ default: module.DiBootstrap }))),
-  Swagger: React.lazy(() => import("react-icons/si").then(module => ({ default: module.SiSwagger }))),
-  ApiGoogleBooks: React.lazy(() => import("react-icons/si").then(module => ({ default: module.SiGoogle }))),
-  Sequelize: React.lazy(() => import("react-icons/si").then(module => ({ default: module.SiSequelize }))),
-  NodeJS: React.lazy(() => import("react-icons/di").then(module => ({ default: module.DiNodejs }))),
-  Axios: React.lazy(() => import("react-icons/si").then(module => ({ default: module.SiAxios }))),
+  Flutter: React.lazy(() => import("react-icons/si").then(m => ({ default: m.SiFlutter }))),
+  MySQL: React.lazy(() => import("react-icons/di").then(m => ({ default: m.DiMysql }))),
+  Express: React.lazy(() => import("react-icons/si").then(m => ({ default: m.SiExpress }))),
+  Kotlin: React.lazy(() => import("react-icons/si").then(m => ({ default: m.SiKotlin }))),
+  Php: React.lazy(() => import("react-icons/di").then(m => ({ default: m.DiPhp }))),
+  Java: React.lazy(() => import("react-icons/fa").then(m => ({ default: m.FaJava }))),
+  Html: React.lazy(() => import("react-icons/di").then(m => ({ default: m.DiHtml5 }))),
+  Css: React.lazy(() => import("react-icons/di").then(m => ({ default: m.DiCss3 }))),
+  Javascript: React.lazy(() => import("react-icons/di").then(m => ({ default: m.DiJavascript1 }))),
+  Vuejs: React.lazy(() => import("react-icons/si").then(m => ({ default: m.SiVuedotjs }))),
+  React: React.lazy(() => import("react-icons/di").then(m => ({ default: m.DiReact }))),
+  Bootstrap: React.lazy(() => import("react-icons/di").then(m => ({ default: m.DiBootstrap }))),
+  Swagger: React.lazy(() => import("react-icons/si").then(m => ({ default: m.SiSwagger }))),
+  ApiGoogleBooks: React.lazy(() => import("react-icons/si").then(m => ({ default: m.SiGoogle }))),
+  Sequelize: React.lazy(() => import("react-icons/si").then(m => ({ default: m.SiSequelize }))),
+  NodeJS: React.lazy(() => import("react-icons/di").then(m => ({ default: m.DiNodejs }))),
+  Axios: React.lazy(() => import("react-icons/si").then(m => ({ default: m.SiAxios }))),
 };
-
-// Types props
 interface ProjectCardProps {
   imgPath: string;
   altText: string;
@@ -40,50 +38,67 @@ interface ProjectCardProps {
   youtubeLink?: string;
   isGitLab?: boolean;
 }
-
-function ProjectCards(props: ProjectCardProps) {
+// TechStackIcons component
+function TechStackIcons({ techStack }: { techStack: string[] }) {
+  return (
+    <Row className="pb-2 justify-content-center">
+      {techStack.map((tech, index) => {
+        const IconComponent = techIcons[tech];
+        return IconComponent ? (
+          <Col key={index} xs={4} md={2} className="tech-icons2" data-tooltip-id="tooltip" data-tooltip-content={tech}>
+            <Suspense fallback={<div style={{ height: "32px" }} />}>
+              <IconComponent aria-label={tech} title={tech} role="img" tabIndex={0} />
+            </Suspense>
+          </Col>
+        ) : null;
+      })}
+    </Row>
+  );
+}
+// ProjectCard component
+function ProjectCard(props: ProjectCardProps) {
   const { t } = useTranslation();
 
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt={`${props.title} project image`} loading="lazy" decoding="async" />
+      <div className="project-img-wrapper">
+        <Card.Img
+          variant="top"
+          src={props.imgPath}
+          alt={props.altText}
+          loading="lazy"
+          decoding="async"
+          className="project-img"
+        />
+      </div>
       <Card.Body>
-        <Card.Title><h3><strong className="blue">{props.title}</strong></h3></Card.Title>
+        <Card.Title>
+          <h3><strong className="blue">{props.title}</strong></h3>
+        </Card.Title>
 
-        <Row className="pb-2" style={{ justifyContent: "center" }}>
-          {props.techStack.map((tech, index) => {
-            const IconComponent = techIcons[tech];
-            return IconComponent ? (
-              <Col key={index} xs={4} md={2} className="tech-icons2" data-tooltip-id="tooltip" data-tooltip-content={tech}>
-                <Suspense fallback={<Spinner animation="border" role="status" />}>
-                  <IconComponent aria-label={tech} />
-                </Suspense>
-              </Col>
-            ) : null;
-          })}
-        </Row>
+        <TechStackIcons techStack={props.techStack} />
 
         <Card.Text className="text-justify">
           {props.description}
         </Card.Text>
 
         <div className="button-group">
-          <Suspense fallback={<Spinner animation="border" role="status" />}>
-            <Button className="button-cv" href={props.ghLink} target="_blank" rel="noopener noreferrer">
-              <BsGithub /> &nbsp;
+          <Suspense fallback={<div style={{ width: "100px", height: "38px" }} />}>
+            <Button className="button-github" href={props.ghLink} target="_blank" rel="noopener noreferrer">
+              <BsGithub style={{ marginRight: "5px", marginBottom: "2px" }} />
               {props.isGitLab ? "GitLab" : "GitHub"}
             </Button>
           </Suspense>
 
-          {!props.isGitLab && props.youtubeLink && (
-            <Suspense fallback={<Spinner animation="border" role="status" />}>
+          {props.youtubeLink && (
+            <Suspense fallback={<div style={{ width: "100px", height: "38px" }} />}>
               <Button
                 className="button-youtube"
                 href={props.youtubeLink}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <BsYoutube /> &nbsp;
+                <BsYoutube style={{ marginRight: "5px", marginBottom: "2px" }} />
                 {t('video')}
               </Button>
             </Suspense>
@@ -94,4 +109,4 @@ function ProjectCards(props: ProjectCardProps) {
   );
 }
 
-export default ProjectCards;
+export default ProjectCard;
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,11 +1,14 @@
-import React, { Suspense } from "react";
+import React, { useState, Suspense } from "react";
+import { Container, Row, Col, Spinner, Button } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
-import { Container, Row, Col, Spinner } from "react-bootstrap";
-import { easeOut, motion } from "framer-motion";
+import { motion } from "framer-motion";
+import Tilt from "react-parallax-tilt";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Utils/Particle";
 import "../../assets/styles/About/About.css";
+import "../../assets/styles/Projet/Projet.css";
 
+// Images
 import lemonmaze from "@image/Projects/LemonMaze.webp";
 import cosmiclink from "@image/Projects/CosmicLink.webp";
 import chrono from "@image/Projects/chrono.webp";
@@ -28,131 +31,72 @@ interface Project {
 }
 
 const projects: Project[] = [
-  {
-    imgPath: portfoliov2,
-    altTextKey: "portfoliov2_image_alt",
-    titleKey: "portfolio_v2_title",
-    descriptionKey: "portfolio_v2_description",
-    ghLink: "https://github.com/Theo22100/Portfolio",
-    techStack: ["React", "Bootstrap", "Css", "NodeJS", "Axios"],
-  },
-  {
-    imgPath: apibook,
-    altTextKey: "apibook_image_alt",
-    titleKey: "library_title",
-    descriptionKey: "library_description",
-    ghLink: "https://github.com/Theo22100/MDS-M1-Librairie",
-    techStack: ["Express", "Swagger", "MySQL", "React", "Bootstrap", "Sequelize"],
-  },
-  {
-    imgPath: lemonmaze,
-    altTextKey: "lemonmaze_image_alt",
-    titleKey: "lemonmaze_api_title",
-    descriptionKey: "lemonmaze_api_description",
-    ghLink: "https://github.com/Theo22100/APILemonMaze",
-    techStack: ["Express", "Swagger", "MySQL"],
-  },
-  {
-    imgPath: lemonmaze,
-    altTextKey: "lemonmaze_image_alt",
-    titleKey: "lemonmaze_title",
-    descriptionKey: "lemonmaze_description",
-    ghLink: "https://github.com/Theo22100/Lemon_Maze",
-    techStack: ["Flutter"],
-  },
-  {
-    imgPath: cosmiclink,
-    altTextKey: "cosmiclink_image_alt",
-    titleKey: "cosmiclink_title",
-    descriptionKey: "cosmiclink_description",
-    ghLink: "https://github.com/Theo22100/CosmicLink",
-    youtubeLink: "https://youtube.com/watch?v=3yVybmKT5d0",
-    techStack: ["Php", "Javascript", "Html", "Css"],
-  },
-  {
-    imgPath: chrono,
-    altTextKey: "chrono_image_alt",
-    titleKey: "minuteur_mobile_title",
-    descriptionKey: "minuteur_mobile_description",
-    ghLink: "https://github.com/Theo22100/MDS-ExamMobile2024",
-    techStack: ["Kotlin"],
-  },
-  {
-    imgPath: breizhcoin,
-    altTextKey: "breizhcoin_image_alt",
-    titleKey: "breizhcoin_title",
-    descriptionKey: "breizhcoin_description",
-    ghLink: "https://github.com/Theo22100/BreizhCoin",
-    youtubeLink: "https://youtu.be/OqgS7SW_8tU",
-    techStack: ["Php", "Javascript", "Html", "Css"],
-  },
-  {
-    imgPath: tboi,
-    altTextKey: "tboi_image_alt",
-    titleKey: "thebindingofisaac_title",
-    descriptionKey: "thebindingofisaac_description",
-    ghLink: "https://github.com/Theo22100/TheBindingOfIsaac",
-    techStack: ["Java"],
-  },
-  {
-    imgPath: portfolio,
-    altTextKey: "portfolio_image_alt",
-    titleKey: "portfolio_v1_title",
-    descriptionKey: "portfolio_v1_description",
-    ghLink: "https://github.com/Theo22100/Old-PortFolio",
-    techStack: ["Vuejs", "Javascript", "Html", "Css"],
-  },
+  { imgPath: portfoliov2, altTextKey: "portfoliov2_image_alt", titleKey: "portfolio_v2_title", descriptionKey: "portfolio_v2_description", ghLink: "https://github.com/Theo22100/Portfolio", techStack: ["React", "Bootstrap", "Css", "NodeJS", "Axios"] },
+  { imgPath: apibook, altTextKey: "apibook_image_alt", titleKey: "library_title", descriptionKey: "library_description", ghLink: "https://github.com/Theo22100/MDS-M1-Librairie", techStack: ["Express", "Swagger", "MySQL", "React", "Bootstrap", "Sequelize"] },
+  { imgPath: lemonmaze, altTextKey: "lemonmaze_image_alt", titleKey: "lemonmaze_api_title", descriptionKey: "lemonmaze_api_description", ghLink: "https://github.com/Theo22100/APILemonMaze", techStack: ["Express", "Swagger", "MySQL"] },
+  { imgPath: lemonmaze, altTextKey: "lemonmaze_image_alt", titleKey: "lemonmaze_title", descriptionKey: "lemonmaze_description", ghLink: "https://github.com/Theo22100/Lemon_Maze", techStack: ["Flutter"] },
+  { imgPath: cosmiclink, altTextKey: "cosmiclink_image_alt", titleKey: "cosmiclink_title", descriptionKey: "cosmiclink_description", ghLink: "https://github.com/Theo22100/CosmicLink", youtubeLink: "https://youtube.com/watch?v=3yVybmKT5d0", techStack: ["Php", "Javascript", "Html", "Css"] },
+  { imgPath: chrono, altTextKey: "chrono_image_alt", titleKey: "minuteur_mobile_title", descriptionKey: "minuteur_mobile_description", ghLink: "https://github.com/Theo22100/MDS-ExamMobile2024", techStack: ["Kotlin"] },
+  { imgPath: breizhcoin, altTextKey: "breizhcoin_image_alt", titleKey: "breizhcoin_title", descriptionKey: "breizhcoin_description", ghLink: "https://github.com/Theo22100/BreizhCoin", youtubeLink: "https://youtu.be/OqgS7SW_8tU", techStack: ["Php", "Javascript", "Html", "Css"] },
+  { imgPath: tboi, altTextKey: "tboi_image_alt", titleKey: "thebindingofisaac_title", descriptionKey: "thebindingofisaac_description", ghLink: "https://github.com/Theo22100/TheBindingOfIsaac", techStack: ["Java"] },
+  { imgPath: portfolio, altTextKey: "portfolio_image_alt", titleKey: "portfolio_v1_title", descriptionKey: "portfolio_v1_description", ghLink: "https://github.com/Theo22100/Old-PortFolio", techStack: ["Vuejs", "Javascript", "Html", "Css"] }
 ];
 
 const Projects: React.FC = () => {
   const { t } = useTranslation();
+  const [selectedFilter, setSelectedFilter] = useState("All");
+  const techFilters = ["All", ...Array.from(new Set(projects.flatMap(p => p.techStack)))];
+
+  const filteredProjects = selectedFilter === "All" ? projects : projects.filter(p => p.techStack.includes(selectedFilter));
 
   return (
     <Container fluid className="project-section">
       <Particle />
       <Container>
-
-        <motion.h1
-          className="custom-title py-4"
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-
-          transition={{ duration: 0.8, ease: easeOut }}
-        >
-          {t("my_projects")}{" "}{t("projects")}
+        <motion.h1 className="custom-title pb-3" initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8, ease: "easeOut" }}>
+          {t("my_projects")} {t("projects")}
         </motion.h1>
-        <p className="pb-3" style={{ fontSize: "1.2em", color: "white", textDecoration: "underline" }}>{t("projects_description")}</p>
-        <Row className="pb-1" style={{ justifyContent: "center", alignItems: "stretch" }}>
-          {projects.map((project, index) => (
-            <Col
-              md={4}
-              key={index}
-              className="project-card py-4 px-4"
-              style={{ display: "flex", height: "100%" }}
+
+        <div className="filter-buttons d-flex flex-wrap gap-2 justify-content-center mb-4">
+          {techFilters.map((filter, index) => (
+            <motion.div
+              key={filter}
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.4, delay: index * 0.1 }}
             >
-              <motion.div
-                initial={{ opacity: 0, scale: 0.9, y: 30 }}
-                animate={{ opacity: 1, scale: 1, y: 0 }}
-                transition={{
-                  duration: 0.6,
-                  ease: "easeOut",
-                  delay: index * 0.1,
-                }}
+              <Button
+                variant={selectedFilter === filter ? "primary" : "outline-light"}
+                onClick={() => setSelectedFilter(filter)}
               >
-                <ProjectCard
-                  imgPath={project.imgPath}
-                  altText={t(project.altTextKey)}
-                  title={t(project.titleKey)}
-                  description={t(project.descriptionKey)}
-                  ghLink={project.ghLink}
-                  youtubeLink={project.youtubeLink}
-                  techStack={project.techStack}
-                />
+                {filter}
+              </Button>
+            </motion.div>
+          ))}
+        </div>
+
+
+        <Row className="justify-content-center align-items-stretch">
+          {filteredProjects.map((project, index) => (
+            <Col xs={12} sm={6} md={4} key={index} className="project-card px-3 py-4">
+              <motion.div initial={{ opacity: 0, scale: 0.9, y: 30 }} animate={{ opacity: 1, scale: 1, y: 0 }} transition={{ duration: 0.6, ease: "easeOut", delay: index * 0.1 }}>
+                <Tilt glareEnable={true} glareMaxOpacity={0.1} scale={1.03} transitionSpeed={2500} tiltMaxAngleX={6} tiltMaxAngleY={6}>
+                  <ProjectCard
+                    imgPath={project.imgPath}
+                    altText={t(project.altTextKey)}
+                    title={t(project.titleKey)}
+                    description={t(project.descriptionKey)}
+                    ghLink={project.ghLink}
+                    youtubeLink={project.youtubeLink}
+                    techStack={project.techStack}
+                  />
+                </Tilt>
               </motion.div>
             </Col>
           ))}
         </Row>
       </Container>
+
       <Suspense fallback={<Spinner animation="border" role="status" />}>
         <Tooltip id="tooltip" anchorSelect=".tech-icons2" place="top" />
       </Suspense>

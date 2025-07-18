74bbff7d02658b305c2dc5334075867dfeecc95b
Theo35000 <theo@nahibu.com>
2025-07-18 16:45:50 +0200

    fix: improve scroll behavior in ScrollToTop component

--- src/components/Utils/ScrollToTop.tsx
+++ src/components/Utils/ScrollToTop.tsx
@@ -1,12 +1,11 @@
-import { useLocation} from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
-
 
 function ScrollToTop(): null {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
   }, [pathname]);
 
   return null;

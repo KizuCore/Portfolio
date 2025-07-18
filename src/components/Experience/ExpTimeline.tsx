0dc037cdac56ec75033e1e73dcfbcd6b7c46654c
Theo35000 <theo@nahibu.com>
2025-07-18 14:58:34 +0200

    refactor: adjust timeline styles and enhance animation with new arrow component

--- src/components/Experience/ExpTimeline.tsx
+++ src/components/Experience/ExpTimeline.tsx
@@ -5,8 +5,11 @@ import { useTimelineData } from "./data/timelineData";
 import TimelineItem from "./TimelineItem.tsx";
 import '../../assets/styles/Experience/Experience.css';
 import Particle from "../Utils/Particle.tsx";
+import { motion } from "framer-motion";
+import { useTranslation } from "react-i18next";
 
 const Experience = () => {
+  const { t } = useTranslation();
   const timelineData = useTimelineData();
   const timelineRef = useRef<HTMLDivElement>(null);
   const scrollPercentage = useScrollProgress(timelineRef);
@@ -19,7 +22,23 @@ const Experience = () => {
     <Container>
       <Particle />
       <Container fluid className="timeline-container">
+        <motion.h1
+          className="custom-title pb-5 mb-5"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+        >
+          {t("Mon parcours")}
+        </motion.h1>
         <div className="timeline" ref={timelineRef}>
+          <div className="timeline-arrow-up-wrapper">
+            <svg className="timeline-chevron" viewBox="0 0 24 24">
+              <polyline points="6 15 12 9 18 15" />
+            </svg>
+          </div>
+
+
+
           <div className="timeline-line" style={{ height: `${scrollPercentage}%` }} />
           {timelineData.map((item, index) => (
             <TimelineItem key={index} item={item} isLeft={index % 2 === 0} />

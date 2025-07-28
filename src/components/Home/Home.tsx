f3a4e00b9bd4131e0ea001caf64e63ec8af0c6dc
Theo35000 <theo@nahibu.com>
2025-07-28 16:42:20 +0200

    fix: update event horizon element to self-closing tag in Home.tsx

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -95,7 +95,7 @@ function Home(): JSX.Element {
             <Col ref={ref} xs={12} md={6} className="d-flex justify-content-center align-items-center py-4">
               <div className="logo-wrapper mt-3 mt-md-0">
                 <div className="black-hole-realistic">
-                  <div className="event-horizon"></div>
+                  <div className="event-horizon" />
                 </div>
 
 

21b70a979442d581aa712c21f8f0dd331512c874
KizuCore <theo.guerin35000@gmail.com>
2026-08-28 16:19:06 +0200

    feat: add TypeScript definitions for react-icons modules

---
+++ src/types/react-icons-all-files.d.ts
@@ -0,0 +1,50 @@
+declare module "@react-icons/all-files/si/*" {
+  import type { ComponentType, SVGProps } from "react";
+
+  type ReactIcon = ComponentType<SVGProps<SVGSVGElement>>;
+
+  export const SiAdobephotoshop: ReactIcon;
+  export const SiAdobepremiere: ReactIcon;
+  export const SiAmazonaws: ReactIcon;
+  export const SiAndroidstudio: ReactIcon;
+  export const SiAngular: ReactIcon;
+  export const SiBootstrap: ReactIcon;
+  export const SiConfluence: ReactIcon;
+  export const SiCss3: ReactIcon;
+  export const SiCplusplus: ReactIcon;
+  export const SiDocker: ReactIcon;
+  export const SiFigma: ReactIcon;
+  export const SiFlask: ReactIcon;
+  export const SiFlutter: ReactIcon;
+  export const SiGit: ReactIcon;
+  export const SiGithub: ReactIcon;
+  export const SiGitlab: ReactIcon;
+  export const SiGnubash: ReactIcon;
+  export const SiGraphql: ReactIcon;
+  export const SiHeroku: ReactIcon;
+  export const SiHtml5: ReactIcon;
+  export const SiJenkins: ReactIcon;
+  export const SiJira: ReactIcon;
+  export const SiKotlin: ReactIcon;
+  export const SiLaravel: ReactIcon;
+  export const SiLinux: ReactIcon;
+  export const SiMariadb: ReactIcon;
+  export const SiNetlify: ReactIcon;
+  export const SiNginx: ReactIcon;
+  export const SiNuxtDotJs: ReactIcon;
+  export const SiPostgresql: ReactIcon;
+  export const SiPostman: ReactIcon;
+  export const SiRust: ReactIcon;
+  export const SiSwift: ReactIcon;
+  export const SiTailwindcss: ReactIcon;
+  export const SiTrello: ReactIcon;
+  export const SiTypescript: ReactIcon;
+}
+
+declare module "@react-icons/all-files/vsc/*" {
+  import type { ComponentType, SVGProps } from "react";
+
+  type ReactIcon = ComponentType<SVGProps<SVGSVGElement>>;
+
+  export const VscFileCode: ReactIcon;
+}
